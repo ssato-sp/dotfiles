22a055b629235fa9b1acd532c14acdd89b380da0
ssato-sp <shunichisato7@gmail.com>
2020-08-17 16:26:42 +0900

    enable ale on vim

--- .bin/make_pyenv_neovim_ubuntu.sh
+++ .bin/make_pyenv_neovim_ubuntu.sh
@@ -4,6 +4,10 @@ pyenv install 3.8.2
 pyenv virtualenv 3.8.2 python3_neovim
 pyenv global python3_neovim
 pip install --upgrade pip
-pip install pynvim
-pip install flake8
+pip install -U pynvim
+pip install -U flake8
+pip install -U flake8-import-order
+pip install -U autopep8
+pip install -U black
+pip install -U isort
 pyenv global 3.8.2
